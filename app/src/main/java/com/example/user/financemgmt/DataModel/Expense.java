a7e06a902c6d0d1a3ff71dfdc7831a6ca05eab55
Palibin <switch_h13@mail.ru>
2017-10-02 22:23:38 +0300

    very small commit

--- app/src/main/java/com/example/user/financemgmt/DataModel/Expense.java
+++ app/src/main/java/com/example/user/financemgmt/DataModel/Expense.java
@@ -1,5 +1,7 @@
 package com.example.user.financemgmt.DataModel;
 
+import java.util.GregorianCalendar;
+
 /**
  * Created by Palibin
  * Объект расходов
@@ -7,6 +9,14 @@ package com.example.user.financemgmt.DataModel;
 
 public class Expense {
     private Decreasable source; //объект списания средств
-   // private Usage usage;        //TODO дописать объект Usage(назначение платежа, трата)
-    private long cost;          //Стоимость траты
+    // private Usage usage; //TODO дописать объект Usage(категория назначение платежа, трата)
+    private long cost; //Стоимость траты
+    GregorianCalendar payDate; //Дата совершение платежа
+
+    public Expense(Decreasable source, long cost, GregorianCalendar Date /*, Usage usage*/) {
+        this.source = source;
+        this.cost = cost;
+        this.payDate=Date;
+        //this.usage = usage;
+    }
 }
