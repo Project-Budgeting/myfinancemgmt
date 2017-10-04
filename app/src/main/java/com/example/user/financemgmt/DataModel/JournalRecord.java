46540b278f3860672e930422a63949c687800b1e
Palibin <switch_h13@mail.ru>
2017-10-05 00:02:18 +0300

    Функционал по работе с хранилищем cashStorage работает некорректно, удалены классы статистики, их функционал добавлен в DriverDao в блоке для работы с JournalRecordsStorage

--- app/src/main/java/com/example/user/financemgmt/DataModel/JournalRecord.java
+++ app/src/main/java/com/example/user/financemgmt/DataModel/JournalRecord.java
@@ -20,14 +20,20 @@ public class JournalRecord {
     private String additionalSettings;
     private TypesOfCashObjects type; //тип события
     private GregorianCalendar date; //дата наступления события
+    private long id; // id Записи в журнале
+    private long idOfEvent; // id события
+    private static long count=0;
 
-    public JournalRecord(long amount, String name, String additionalSettings) {
+    public JournalRecord(long amount, String name, String additionalSettings, long idOfEvent) {
         this.amount = amount;
         this.name = name;
         this.additionalSettings = additionalSettings;
+        this.idOfEvent = idOfEvent;
+        this.id = generateid();
+        count++;
     }
-    /*конструктор для тестирования*/
 
+    /*конструктор для тестирования*/
     public JournalRecord(long amount, String name, String additionalSettings, TypesOfCashObjects type) {
         this.amount = amount;
         this.name = name;
@@ -35,23 +41,40 @@ public class JournalRecord {
         this.type = type;
     }
 
+    private long generateid(){
+        return count+1;
+    }
+
     public TypesOfCashObjects getType() {
         return type;
+    }
+
+    public long getIdOfEvent() {
+        return idOfEvent;
     }
 
     public GregorianCalendar getDate() {
         return date;
     }
 
+    public long getAmount() {
+        return amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public static JournalRecord makeRecordInJournal(Object event, long amount,
-                                                    String name, String additionalSettings ){
-        JournalRecord jr = new JournalRecord(amount, name, additionalSettings);
-       //TODO переписать так, чтобы дата создавалась только в формате Год, месяц, день
-        jr.date = new GregorianCalendar();
+                                                    String name, String additionalSettings, long idOfEvent ){
+        JournalRecord jr = new JournalRecord(amount, name, additionalSettings, idOfEvent);
+        GregorianCalendar dateOfNow = new GregorianCalendar();
+        jr.date = new GregorianCalendar(dateOfNow.get(Calendar.YEAR), dateOfNow.get(Calendar.MONTH), dateOfNow.get(Calendar.DAY_OF_MONTH));
         switch (event.getClass().getName()) {
             case "CashSource":  jr.type = TypesOfCashObjects.CASH_SOURCE;
                                 break;
             case "Usage":       jr.type = TypesOfCashObjects.USAGE;
+                                break;
         }
         DriverDao.insertRecordInJournal(jr);
         return jr;
@@ -77,10 +100,8 @@ public class JournalRecord {
     //Сравнить две даты по полям Год, Месяц, День меяца.
     //TODO проверить работоспособность метода
     public static boolean compareDatesByYMD(GregorianCalendar date1, GregorianCalendar date2){
-        if ((date1.YEAR==date2.YEAR) &
-            (date1.MONTH==date2.MONTH) &
-            (date1.DAY_OF_MONTH==date2.DAY_OF_MONTH))
-            return true;
-        else return false;
+        return ((date1.get(Calendar.YEAR)==date2.get(Calendar.YEAR)) &
+            (date1.get(Calendar.MONTH)==date2.get(Calendar.MONTH)) &
+            (date1.get(Calendar.DAY_OF_MONTH)==date2.get(Calendar.DAY_OF_MONTH)));
     }
 }
