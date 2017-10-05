be7ae36a5effa3210d071081d65344f996c53598
Oleg <01e6@rambler.ru>
2017-10-05 17:27:46 +0400

    Start Butter Knife refactoring

--- app/src/main/java/com/example/user/financemgmt/MainActivity.java
+++ app/src/main/java/com/example/user/financemgmt/MainActivity.java
@@ -9,30 +9,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 
-
-    public class MainActivity extends AppCompatActivity {
-
-        private TabLayout tabLayout;
-        private ViewPager viewPager;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            viewPager = (ViewPager) findViewById(R.id.mainActivityPager);
-            setupViewPager(viewPager);
-
-            tabLayout = (TabLayout) findViewById(R.id.tablayout);
-            tabLayout.setupWithViewPager(viewPager);
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
-        }
+public class MainActivity extends AppCompatActivity {
 
-        private void setupViewPager(ViewPager viewPager) {
-            ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-            adapter.addFragment(new TestFragment(), "Управление Средствами");
-            adapter.addFragment(new TestFragment2(), "Журнал расходов");
-            viewPager.setAdapter(adapter);
-        }
+    /*private TabLayout tabLayout;
+      private ViewPager viewPager;*/
+
+    @BindView(R.id.mainActivityPager) //Butter Knife Refactoring
+            ViewPager viewPager; //Butter Knife Refactoring
+    @BindView(R.id.tablayout) //Butter Knife Refactoring
+            TabLayout tabLayout; //Butter Knife Refactoring
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this); //Butter Knife Refactoring
+
+         /*viewPager = (ViewPager) findViewById(R.id.mainActivityPager);*/
+        setupViewPager(viewPager);
+        /*tabLayout = (TabLayout) findViewById(R.id.tablayout);*/
+        tabLayout.setupWithViewPager(viewPager);
+
+
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TestFragment(), "Управление Средствами");
+        adapter.addFragment(new TestFragment2(), "Журнал расходов");
+        viewPager.setAdapter(adapter);
+    }
+}
